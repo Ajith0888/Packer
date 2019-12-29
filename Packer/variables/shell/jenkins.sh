69b588c59e415e04ded085c494f33d89a929b86d
Ajith0888 <ajithkumar02@gmail.com>
2019-12-29 00:44:38 +0000

    Fixed Issue in Codes

--- Packer/variables/shell/jenkins.sh
+++ Packer/variables/shell/jenkins.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #This script is helps to install Jenkins on Ubuntu
-apt-get update
-apt install openjdk-8-jdk -y
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-apt install jenkins
-systemctl enable jenkins
-systemctl start jenkins
+sleep 300
+sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+#sudo apt-get -y update
+sudo apt-get install -y jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
