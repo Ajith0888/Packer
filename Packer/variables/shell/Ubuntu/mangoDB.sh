4bcb16b7c05ae48d85aeac754e7f6bd7e50b6fa0
Ajith0888 <ajithkumar02@gmail.com>
2019-12-30 03:24:21 +0100

    Updated in code

--- Packer/variables/shell/Ubuntu/mangoDB.sh
+++ Packer/variables/shell/Ubuntu/mangoDB.sh
@@ -4,6 +4,6 @@ sleep 300
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update -y
-sudo apt-get install mongodb-org -y
+sudo apt-get install mongodb-org -y --allow-unauthenticated
 sudo systemctl enable mongod
 sudo systemctl start mongod
