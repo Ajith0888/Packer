40b575a3c0a95325fc0ac08ff5e2e635cbce369f
Ajith0888 <ajithkumar02@gmail.com>
2019-12-30 02:37:42 +0100

    Updated Codes

--- Packer/variables/shell/RHEL/mysql.sh
+++ Packer/variables/shell/RHEL/mysql.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #This script is helps to install MySQL on Suse
-zypper in -y mysql
-systemctl enable mysql
-systemctl start mysql
+sudo yum update -y
+sudo yum -y install @mysql
+sudo fallocate -l 1g /mnt/1GiB.swap
+sudo chmod 600 /mnt/1GiB.swap
+sudo mkswap /mnt/1GiB.swap
+sudo mkswap /mnt/1GiB.swap
+echo '/mnt/1GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
+systemctl enable mysqld
+systemctl start mysqld
